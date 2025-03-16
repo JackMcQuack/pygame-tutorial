1766868ecbef85046e2292e197acb17713155327
Cam McHugh <cam.mchugh@ncsmartcall.com>
2025-03-16 13:02:52 -0600

    First working version of the script.

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Pygame Demo")
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))
+test_surface.fill((255, 0, 255))
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    screen.blit(test_surface, (0, 0))
+
+    pygame.display.update()
+    clock.tick(60)
